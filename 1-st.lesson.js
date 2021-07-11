1bcc232351797dbebcb1822ecaa9d69e6166c10a
liza-mantashyan <lizamantashyan@gmail.com>
2021-07-11 08:01:02 -0700

    setTimeout3000

--- 1-st.lesson.js
+++ 1-st.lesson.js
@@ -1,4 +1,6 @@
 console.log("master-branch");
 setTimeout(()=>console.log("my first node project"), 1000);
 setTimeout(()=>console.log("my first node project"), 2000);
+setTimeout(()=>console.log("my first node project"), 3000);
+
 
